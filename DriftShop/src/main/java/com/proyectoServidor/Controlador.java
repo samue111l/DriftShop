64f112ab5499f14837f3720187cddf1079e86f70
samue111l <samuelpblazquez@gmail.com>
2024-02-01 12:45:20 +0100

    modelobd

--- DriftShop/src/main/java/com/proyectoServidor/Controlador.java
+++ DriftShop/src/main/java/com/proyectoServidor/Controlador.java
@@ -3,26 +3,22 @@ package com.proyectoServidor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class Controlador {
 
-    private List<Producto> productos = new ArrayList<>();
+    private ModeloDB modeloDB;
 
-    // Constructor to initialize productos
-    public Controlador() {
-        productos.add(new Producto("motor1"));
-        productos.add(new Producto("motor2"));
-        productos.add(new Producto("motor3"));
+    public Controlador(ModeloDB modeloDB) {
+        this.modeloDB = modeloDB;
     }
 
     @GetMapping("/listar")
     public String mostrarVista(Model model) {
+        List<Producto> productos = modeloDB.listarProductos();
         model.addAttribute("productos", productos);
         return "index";
     }
@@ -31,7 +27,7 @@ public class Controlador {
     public String tienda(@RequestParam String name) {
         return "redirect:/";
     }
-    
+
     @GetMapping("/taller")
     public String taller(@RequestParam String name) {
         return "redirect:/";
